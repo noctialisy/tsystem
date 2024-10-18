42b70f2d73f1cd5dcc27ccb6cbb1ec527849cbc9
noctialisy <1392499+noctialisy@users.noreply.github.com>
2024-10-18 08:16:57 +0200

    Add personal LogPath to DISM operations

--- modules/alacrisys.system.scan.ps1
+++ modules/alacrisys.system.scan.ps1
@@ -1,6 +1,8 @@
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /CheckHealth}; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /ScanHealth}; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /AnalyzeComponentStore}; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /StartComponentCleanup}; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /RestoreHealth}; $j | Wait-Job;
+$logPath = "$($PSScriptRoot)\..\logs\Dism.log";
+
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /CheckHealth /LogPath:$input} -InputObject $logPath; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /ScanHealth/LogPath:$input} -InputObject $logPath; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /AnalyzeComponentStore/LogPath:$input} -InputObject $logPath; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /StartComponentCleanup/LogPath:$input} -InputObject $logPath; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /RestoreHealth/LogPath:$input} -InputObject $logPath; $j | Wait-Job;
 $j = Start-Job -ScriptBlock {SFC /ScanNow}; $j | Wait-Job;
