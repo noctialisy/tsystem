39578ed80d64d6e803bdb9b49cf238c2f3d06851
noctialisy <1392499+noctialisy@users.noreply.github.com>
2024-10-18 08:16:59 +0200

    Add video_merge and stream_markers_convert functions

--- tsystem.py
+++ tsystem.py
@@ -1,4 +1,5 @@
 import sys, os, subprocess, datetime
+import json, csv
 
 class SystemHandler:
     commands = []
@@ -22,6 +23,38 @@ class SystemHandler:
                     self.netframework_install()
                 elif command == 'netframework_build':
                     self.netframework_build()
+                elif 'video_merge' in command:
+                    if ',' not in command:
+                        print('video_merge requires specifications, please use the video_merge,<video_path>,<video_ext> syntax')
+                        return
+                    
+                    command_part = command.split(',')
+                    video_path = './'
+                    video_ext = 'mkv'
+
+                    if command_part[1] != '':
+                        video_path = command_part[1]
+
+                    if command_part[2] != '':
+                        video_ext = command_part[2]
+
+                    self.video_merge(video_path, video_ext)
+
+                elif 'stream_markers_convert' in command:
+                    if ',' not in command:
+                        print('stream_markers_convert requires specifications, please use the stream_markers_convert,<file_path> syntax')
+                        return
+                    
+                    command_part = command.split(',')
+                    file_path = './'
+
+                    if command_part[1] != '':
+                        file_path = command_part[1]
+
+                    self.stream_markers_convert(file_path)
+
+
+
 
     # Module definition
     def system_clean(self):
@@ -44,6 +77,7 @@ class SystemHandler:
         log_file = './logs/Assessment_' + str(self.date.timestamp()) + '.xml'
         subprocess.run(['WinSat', 'formal', '-restart', 'clean', '-v', '-xml ' + log_file])
 
+
     def netframework_install(self):
         netframework_folder = './vcredist'
         netframework_content = os.scandir(os.fspath(netframework_folder))
@@ -65,6 +99,84 @@ class SystemHandler:
             for file in files:
                 if file.endswith('ngen.exe'):
                     subprocess.run([os.path.join(root, file), 'update'])
+
+    
+    # Merges multiple video segments into a single output
+    def video_merge(self, path, ext):
+        if path == '':
+            print('This command requires a path specification')
+            return
+
+        if 'mkv' not in ext and 'mp4' not in ext:
+            print('Missing or not supported video extention')
+            return
+        
+        print('Merging ' + ext + ' files in ' + path + '...\n')
+        
+        with open(os.path.realpath(path) + '\\output_video_merges.tmp', 'w+', encoding='utf8') as output_video_merges:
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    if file.endswith(ext):
+                        output_video_merges.write('file \'' + os.path.join(root, file) + '\'\n')
+
+        
+        subprocess.run('ffmpeg -f concat -safe 0 -i '+ os.path.realpath(path) + '\\output_video_merges.tmp -map 0 -c copy ' + os.path.realpath(path) + '\\output.' + ext)
+
+
+    # Used to convert Twitch stream markers to kdenlive usable markers
+    def stream_markers_convert(self, path):
+        if path == '' or 'csv' not in path:
+            print('This command requires a csv path specification')
+            return
+        
+        print('Converting streamer markers from ' + path + '...\n')
+        
+        output_path = path[:-4] + '.json'
+        container = []
+        tmp_content = ''
+
+        # Read and parse origin csv file
+        with open(path, newline='', encoding='utf-8', mode='r+') as csv_file:
+            content = csv_file.read()
+
+            if 'time,a,b,c\n' not in content:
+                csv_file.seek(0)
+                tmp_content = 'time,a,b,c\n' + content
+                csv_file.write(tmp_content)
+
+            csv_file.seek(0)
+
+            reader = csv.DictReader(csv_file)
+
+            for row in reader:
+                timer = row['time']
+                split_timer = timer.split(':')
+
+                hours = int(split_timer[0])
+                minutes = int(split_timer[1])
+                seconds = int(split_timer[2])
+
+                pos = (seconds * 60) + (minutes * 60 * 60) + (hours * 60 * 60 * 24)
+
+                item = {
+                    'pos': pos,
+                    'comment': 'Automatically generated time',
+                    'type': 0
+
+                }
+
+                container.append(item)
+        
+        # outs converted content to json
+        with open(output_path, encoding='utf-8', mode='w') as json_file:
+            json.dump(container, json_file)
+
+
+
+
+
+
+        
 
 
 # Def Main
