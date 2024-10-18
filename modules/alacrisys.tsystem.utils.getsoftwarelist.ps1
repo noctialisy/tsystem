8186ae352e2503a244a1d61a44dbc246d678c6ce
noctialisy <1392499+noctialisy@users.noreply.github.com>
2024-10-18 08:16:57 +0200

    Fix path on getsoftware script

--- modules/alacrisys.tsystem.utils.getsoftwarelist.ps1
+++ modules/alacrisys.tsystem.utils.getsoftwarelist.ps1
@@ -1,17 +1,19 @@
-Remove-Item "$($PSScriptRoot)\ActualSoftwareListScript.txt";
+$logFileName = "ActualSoftwareListScript.txt";
+$logPath = "$($PSScriptRoot)\..\logs\$($logFileName)";
+Remove-Item "$($logPath)";
 
 $programList = Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" | Select-Object DisplayName, DisplayVersion;
-Write-Output $programList >> "$($PSScriptRoot)\ActualSoftwareList.txt";
+Write-Output $programList >> "$($logPath)";
 
 $i = 0;
 while ($i -le $programList.Length) {
     if ($programList[$i].DisplayName -and $programList[$i].DisplayVersion) {
         $name = $programList[$i].DisplayName;
         $version = $programList[$i].DisplayVersion;
-        Write-Output "$($name)|$($version)" >> "$($PSScriptRoot)\ActualSoftwareListScript.txt";
+        Write-Output "$($name)|$($version)" >> "$($logPath)";
     }
     $i++;
 }
-"EOF|0.0.0" >> "$($PSScriptRoot)\ActualSoftwareListScript.txt";
+"EOF|0.0.0" >> "$($logPath)";
 
-Remove-Item "$($PSScriptRoot)\ActualSoftwareList.txt";
+#Remove-Item "$($logPath)";
