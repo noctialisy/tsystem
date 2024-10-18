442a980bed7a74350a0662757f9ff9f78cc3e75d
noctialisy <1392499+noctialisy@users.noreply.github.com>
2024-10-18 08:16:54 +0200

    Remove old alias script

--- modules/alacrisys.tsystem.listmodules.ps1
+++
@@ -1,5 +0,0 @@
-$driveVolumeLetter = Get-Volume -FileSystemLabel "Data_Unit" | Select-Object -Property "DriveLetter" | Select -ExpandProperty "DriveLetter";
-
-#Lists custom modules present on the system
-
-dir "$($driveVolumeLetter):\WorkSpace_Global\Tools\TSystem\Modules"
