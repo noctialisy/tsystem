e903769da64a33eaffc14a65b2ac757b5c77616d
noctialisy <1392499+noctialisy@users.noreply.github.com>
2024-10-18 08:16:57 +0200

    Fix /LogPath switch in system.scan

--- modules/alacrisys.system.scan.ps1
+++ modules/alacrisys.system.scan.ps1
@@ -4,10 +4,10 @@ $logPathSfc = "$($PSScriptRoot)\..\logs\sfc_$($date).log";
 $logPathCbs = "$($PSScriptRoot)\..\logs\cbs_$($date).log";
 
 $j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /CheckHealth /LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /ScanHealth/LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /AnalyzeComponentStore/LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /StartComponentCleanup/LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /RestoreHealth/LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /ScanHealth /LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /AnalyzeComponentStore /LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /StartComponentCleanup /LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /RestoreHealth /LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
 $j = Start-Job -ScriptBlock {SFC /ScanNow}; $j | Wait-Job;
 
 findstr /c:"[SR]" C:\Windows\logs\cbs\cbs.log > $logPathSfc;
