4712acead1479ec02ee464a9684668b68c796c2e
noctialisy <1392499+noctialisy@users.noreply.github.com>
2024-10-18 08:16:57 +0200

    Add SFC log file

--- modules/alacrisys.system.scan.ps1
+++ modules/alacrisys.system.scan.ps1
@@ -1,4 +1,5 @@
 $logPathDism = "$($PSScriptRoot)\..\logs\dism.log";
+$logPathSfc = "$($PSScriptRoot)\..\logs\sfc.log";
 
 $j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /CheckHealth /LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
 $j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /ScanHealth/LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
@@ -6,3 +7,5 @@ $j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /AnalyzeComponentStore/
 $j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /StartComponentCleanup/LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
 $j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /RestoreHealth/LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
 $j = Start-Job -ScriptBlock {SFC /ScanNow}; $j | Wait-Job;
+
+findstr /c:"[SR]" C:\Windows\logs\cbs\cbs.log > $logPathSfc;
