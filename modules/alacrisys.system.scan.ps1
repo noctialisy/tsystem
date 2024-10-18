d5280a0c2d49da4a072cc22a2c1dc56ea425f620
noctialisy <1392499+noctialisy@users.noreply.github.com>
2024-10-18 08:16:57 +0200

    Fix Dism log name

--- modules/alacrisys.system.scan.ps1
+++ modules/alacrisys.system.scan.ps1
@@ -1,8 +1,8 @@
-$logPath = "$($PSScriptRoot)\..\logs\Dism.log";
+$logPathDism = "$($PSScriptRoot)\..\logs\dism.log";
 
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /CheckHealth /LogPath:$input} -InputObject $logPath; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /ScanHealth/LogPath:$input} -InputObject $logPath; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /AnalyzeComponentStore/LogPath:$input} -InputObject $logPath; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /StartComponentCleanup/LogPath:$input} -InputObject $logPath; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /RestoreHealth/LogPath:$input} -InputObject $logPath; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /CheckHealth /LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /ScanHealth/LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /AnalyzeComponentStore/LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /StartComponentCleanup/LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
+$j = Start-Job -ScriptBlock {DISM /Online /Cleanup-Image /RestoreHealth/LogPath:$input} -InputObject $logPathDism; $j | Wait-Job;
 $j = Start-Job -ScriptBlock {SFC /ScanNow}; $j | Wait-Job;
