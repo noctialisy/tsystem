a925ff1370dd4a2754a87600e7f4c9b3844c4777
noctialisy <1392499+noctialisy@users.noreply.github.com>
2024-10-18 08:16:55 +0200

    Remove additional partitions from defrag script

--- modules/alacrisys.system.defrag.ps1
+++ modules/alacrisys.system.defrag.ps1
@@ -1,3 +1,5 @@
 $j = Start-Job -ScriptBlock {defrag C: /O}; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {df64.exe F: /QD}; $j | Wait-Job;
-$j = Start-Job -ScriptBlock {df64.exe G: /QD}; $j | Wait-Job;
+
+#Other drives to optimize
+#$j = Start-Job -ScriptBlock {df64.exe F: /QD}; $j | Wait-Job;
+#$j = Start-Job -ScriptBlock {df64.exe G: /QD}; $j | Wait-Job;
